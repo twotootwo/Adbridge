24637daa0cfe71cc9bbe5f2f224576fa0e59b1ce
twotootwo <138778708+twotootwo@users.noreply.github.com>
2024-11-28 23:37:38 +0900

    Delete nov24 directory

--- nov24/advertiserList/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class RequestedConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'advertiserList'
--- nov24/advertiserList/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-from advertiserList.models import advertiserList
-
-
-class advertiserListForm(forms.ModelForm):
-    class Meta:
-        model=advertiserList
-        fields=('title','nickname')
