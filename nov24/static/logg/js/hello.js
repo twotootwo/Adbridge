e8b3d957fccc886d95a7a22c5528c96f97c344cb
twotootwo <138778708+twotootwo@users.noreply.github.com>
2024-11-29 03:50:53 +0900

    Delete nov24 directory

--- nov24/static/logg/js/hello.js
+++
@@ -1,8 +0,0 @@
-const button = document.getElementsByClassName("home-button")[0]
-
-if(button){
-  button.addEventListener("click", () => {
-    // 버튼 클릭 시 hello.html로 이동
-    window.location.href = "/"
-  });
-}
